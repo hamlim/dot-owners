357638397485d9e1ef99f9437d617adf3e188b29
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-10-06 20:24:09 -0400

    Setup local testing, start work on CLI

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,33 @@
-export const foo = 'bar'
+import k from 'kleur'
+
+interface Args {
+  help?: boolean
+}
+
+let branding = k.bgGreen().white().italic(` [dot-owners] `)
 
 export default function cli() {
-  console.log('Hello world!')
+  let args = process.argv.slice(2).reduce((acc, arg) => {
+    let argName = arg.replace(/--/, '')
+    if (argName.includes('=')) {
+      let [key, val] = argName.split('=')
+      return {
+        ...acc,
+        [key]: val,
+      }
+    }
+    return {
+      ...acc,
+      [argName]: true,
+    }
+  }, {})
+
+  let { help } = args as Args
+
+  if (help) {
+    console.log(`${branding} 👋 Welcome to the .owners CLI`)
+    console.log(`${branding} Usage:\n`)
+
+    return
+  }
 }
